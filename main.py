51f7bf22b7077abe95e19c67e764d1d4e9f38528
Yusuf Arslan <a.online2006@gmail.com>
2026-09-09 13:06:59 +0300

    Froze the parameters ( all weights and biasses for the pretained network ) then replaced the final layer with a new Linear Layer that has random weights

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from torchvision import models  , transforms ,datasets 
+from torchvision import models  , transforms ,datasets ,nn 
 from torch.utils.data import random_split , DataLoader 
+import torch.nn as nn 
 from PIL import Image 
 
 VisionModel  = models.resnet18(weights = models.ResNet18_Weights.DEFAULT ) 
@@ -20,6 +21,14 @@ EURO_DATA_SET = datasets.EuroSAT(root="EUROSAT" , download=True , transform=tran
 
 train_set , test_set = random_split(EURO_DATA_SET , [0.8,0.2] )
 
-train_loader = DataLoader(train_set , 50 )  # splits the data set into batches of 50 and loads them 
-test_loader = DataLoader(test_set,32) 
+train_loader = DataLoader(train_set , batch_size=32, shuffle = True  )  # splits the data set into batches of 50 and loads them 
+test_loader = DataLoader(test_set,batch_size=32 , shuffle = True ) # the batches are fed to eh moddle different in each epoch 
+
+
+# Freeze the prexisting layers , before we create a new final linaer layer with 512 inputs and 10 output neurons 
+
+for parameter in VisionModel.parameters() : 
+    parameter.required_grad = False 
+
+VisionModel.fc = nn.Layer(512,10) 
 
